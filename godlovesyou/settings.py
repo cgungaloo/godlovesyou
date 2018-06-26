a574f1da742078ce7b057fe779ade613617ea4e1
cgungaloo <cgungaloo@yahoo.com>
2018-06-26 22:51:45 +0100

    updated for postgres

--- godlovesyou/settings.py
+++ godlovesyou/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'godlovesyou.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'prequest',
+        'USER': 'postgres',
+        'PASSWORD': 'moses111',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 

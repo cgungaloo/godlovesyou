c752840bb74fe14eae78621a508a626e5919b209
cgungaloo <cgungaloo@yahoo.com>
2018-06-30 22:49:17 +0100

    Added pr listing in views

--- godlovesyouapp/views.py
+++ godlovesyouapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import PrayerRequest
+from django.utils import timezone
 
 # def index(request):
 #     return HttpResponse("God Loves you.")
 
 # Create your views here.
 def prayer_request_list(request):
-    return render(request, 'godlovesyouapp/prayer_request_list.html', {})
+	prs = PrayerRequest.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'godlovesyouapp/prayer_request_list.html', {'prs':prs})

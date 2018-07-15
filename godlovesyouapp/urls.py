b8b13a3d15e4930ac4a578ac17bf9533845581f3
cgungaloo <cgungaloo@yahoo.com>
2018-07-15 18:13:25 +0100

    Added pray list detail

--- godlovesyouapp/urls.py
+++ godlovesyouapp/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from django.conf.urls import url
 from . import views	
 
+# Put urls in order for page matching
+
 urlpatterns =[
-	# path('',views.index, name='index'),
+	url(r'^prayer_request_detail/(?P<pk>\d+)/$',views.prayer_request_detail, name='prayer_request_detail'),
 	url(r'',views.prayer_request_list, name='prayer_request_list'),
 ]
--- godlovesyouapp/views.py
+++ godlovesyouapp/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import PrayerRequest
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+
 
 # def index(request):
 #     return HttpResponse("God Loves you.")
 
 # Create your views here.
 def prayer_request_list(request):
+	print('Heloo there asmlaldanldnlasnd **************')
 	prs = PrayerRequest.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'godlovesyouapp/prayer_request_list.html', {'prs':prs})
+
+def prayer_request_detail(request,pk):
+	pr = get_object_or_404(PrayerRequest, pk=pk)
+	return render(request, 'godlovesyouapp/prayer_request_detail.html', {'pr': pr})
